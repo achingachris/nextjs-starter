53f1c71801e0923bbd02c82cb6c367c565387d2b
chris: wsl <achinga.chris@gmail.com>
2023-04-04 12:38:19 +0300

    pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className='container'>
+      <h1 className='text-center'>Next.Js Starter App</h1>
+
+      <div className='text-center'>
+        <p>Deploy With Vercel:</p>
+        <a href='https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fachingachris%2Fnextjs-starter&project-name=nextjs-starter-project&repository-name=nextjs-starter-project&skippable-integrations=1'>
+          <img src='https://vercel.com/button' alt='Deploy with Vercel' />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Home
