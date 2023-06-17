00a8614e2c286b4d1b90706463cdac2cb0321dc3
chris_using_windows <achinga.chris@gmail.com>
2023-06-17 12:28:56 +0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
